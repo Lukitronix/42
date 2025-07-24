24730e3ad2796eda2fc761a70d7dbae76f5cbb67
Macbook <macbook@4269.com>
2025-07-24 18:58:04 +0200

    Guardando cambios locales antes del rebase

--- Piscina2025/C05/ex04/ft_fibonacci.c
+++ Piscina2025/C05/ex04/ft_fibonacci.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_fibonacci.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lucmunoz <lucmunoz@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: lukitronix <lukitronix@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 17:04:36 by lucmunoz          #+#    #+#             */
-/*   Updated: 2025/07/23 17:31:08 by lucmunoz         ###   ########.fr       */
+/*   Updated: 2025/07/24 18:54:57 by lukitronix       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
 
+#include <stdio.h>
 int	ft_fibonacci(int index)
 {
 	if (index < 0)
@@ -22,12 +22,12 @@ int	ft_fibonacci(int index)
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
 
-/*int	main(void)
+int	main(void)
 {
 	printf("%d\n", ft_fibonacci(-1));
 	printf("%d\n", ft_fibonacci(0));
 	printf("%d\n", ft_fibonacci(1));
-	printf("%d\n", ft_fibonacci(3));
+	printf("%d\n", ft_fibonacci(5));
 	printf("%d\n", ft_fibonacci(10));
 
-}*/
+}
